2e685d760074c30725eb1efce90ea2f5e1c7ce80
ernando93 <ernandokasaluhe93@gmail.com>
2026-06-03 13:06:08 +0700

    Initial Commit

---
+++ MandiriMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MandiriMovie
+//
+//  Created by ITUMAC02 on 03/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
